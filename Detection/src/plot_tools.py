1833f094bd2c7a28f31c0e90ff29bdfa4c5cf7cd
alexandre-lefebv <alexandre.lefebvre@imt-atlantique.net>
2021-01-02 00:42:19 +0100

    Added ellispe plot for 'plot_eddy'

--- Detection/src/plot_tools.py
+++ Detection/src/plot_tools.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""classes.py: This file defines the functions used for ploting eddies and
+streamlines."""
+
+__author__     = "G. Ghienne, A. Lefebvre, A. Lerosey, L. Menard, A. Perier"
+__date__       = "December 2020"
+__license__    = "Libre"
+__version__    = "1.0"
+__maintainer__ = "Not maintened"
+__email__      = ["guillaume.ghienne@imt-atlantique.net",
+                  "alexandre.lefebvre@imt-atlantique.net",
+                  "antoine.lerosey@imt-atlantique.net",
+                  "luc.menard@imt-atlantique.net",
+                  "alexandre.perier@imt-atlantique.net"]
+
 import cartopy
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
+
+from matplotlib.patches import Ellipse
 
 from classes import StreamLine, Eddy
 
@@ -20,7 +38,7 @@ class StreamPlot:
 
     def __init__(self):
         projection = cartopy.crs.PlateCarree()
-        self.fig, self.ax = plt.subplots(1, 1, subplot_kw={'projection': projection})
+        self.fig,self.ax=plt.subplots(1,1,subplot_kw={'projection':projection})
 
         gl = self.ax.gridlines(crs=projection, draw_labels=True)
         gl.xlabels_top, gl.ylabels_right = (False, False)
@@ -65,23 +83,47 @@ class StreamPlot:
         nb_traj = len(trajectories)
         for k in range(nb_traj):
             traj = trajectories[k].coord_list
-            self.ax.plot(traj[:,0], traj[:,1], line_style, transform=cartopy.crs.Geodetic())
+            self.ax.plot(traj[:,0], traj[:,1], line_style,
+                         transform=cartopy.crs.Geodetic())
 
-    def plot_eddies(self, eddies, line_style='+'):
+    def plot_eddies(self, eddies, plot_traj=True, line_style='-'):
         """Plots eddies from a .nc particles trajectories file.
+
+        Plot the center of the eddies and an ellipse representing each eddy.
 
         Args:
             eddies (list of Eddy or Eddy) : Eddy or list of Eddy to show.
+            plot_traj (bool, default=True) : If True, plot the trajectories.
             line_style (str) : line style for the trajectories representation.
         """
 
         if type(eddies) == Eddy:
             eddies = [eddies]
 
-        centers = np.array([eddies[k].center for k in range(len(eddies))])
+        n = len(eddies)
+
+        # Plot centers
+        centers  = np.array([eddies[k].center for k in range(n)])
         if len(eddies)>0:
-            self.ax.plot(centers[:,0],centers[:,1], line_style,
+            self.ax.plot(centers[:,0],centers[:,1],'k+',
                          transform=cartopy.crs.Geodetic())
+
+        # Plot trajectories
+        if plot_traj:
+            for k in range(n):
+                self.plot_trajectories(eddies[k].sl_list,line_style=line_style)
+
+        # Plot ellipses
+        # The angle of the ellipse is computed with 'vect_x - vect_y*1j' because of y-axis inversion for plotting ellispses.
+        axes_len = np.array([eddies[k].axis_len for k in range(n)])
+        axes_dir = np.array([eddies[k].axis_dir for k in range(n)])
+        angles =   np.array([np.angle(axes_dir[k,0,0]-axes_dir[k,0,1]*1j) for k
+                             in range(n)])/(2*np.pi)*360
+        for k in range(n):
+            ellipse = Ellipse(centers[k,:],axes_len[k,0]*3,axes_len[k,1]*3,
+                              angle=angles[k],color='black',alpha=1,fill=False,
+                              linewidth=2)
+            self.ax.add_patch(ellipse)
 
     def plot(self,X,Y,line_style='+'):
         self.ax.plot(X,Y,line_style,transform=cartopy.crs.Geodetic())
